4102c72a390054d354aa2f48c71b9bb7772f9968
AndreyArtemov <artwise1966@gmail.com>
2022-08-07 11:55:21 +0300

    Добавил генератор для букв, чисел и спецюсимволов

--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 def home(reguest):
     return render(reguest, 'generator/home.html')
 def password(reguest):
-    return render(reguest, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if reguest.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if reguest.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+'))
+    if reguest.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    lenght = int(reguest.GET.get('lenght', 12))
+
+    thepassword = ''
+    for x in range(lenght):
+        thepassword += random.choice(characters)
+    return render(reguest, 'generator/password.html', {'password':thepassword})
