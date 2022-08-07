b064006de6e46c9e33f5eb7eea2902f10997aa98
AndreyArtemov <artwise1966@gmail.com>
2022-08-07 10:31:35 +0300

    добавлена функция password.html

--- generator/views.py
+++ generator/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(reguest):
-    return HttpResponse('Hello there friend!')
+    return render(reguest, 'generator/home.html')
+def password(reguest):
+    return render(reguest, 'generator/password.html')
